b1e97351f498bfe93af74db747e30892da5fdb35
uzbekprogrammer <mahmudovabdurahim04@gmail.com>
2022-02-28 15:33:36 +0300

    add sqlite

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+#  Created by Abdurahim Mahmudov
+#  Namangan, Uzbekistan
 from aiogram import executor
 
-from loader import dp
+from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -10,6 +12,10 @@ async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
 
+    try:
+        db.create_table_users()
+    except:
+        pass
     # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
 
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,26 @@
+import sqlite3
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from data.config import ADMINS
+from loader import dp, db, bot
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    name = message.from_user.full_name
+    user_id = message.from_user.id
+    # Foydalanuvchini bazaga qoshamiz
+    try:
+        db.add_user(id=message.from_user.id,
+                    name=name)
+        count = db.count_users()[0]
+        msg = f'<a href="tg://user?id={user_id}">{name}</a> bazaga qoshildi. \nBazada {count} ta foydalanuvchi bor.' \
+              f'\nID {user_id}.'
+        await bot.send_message(chat_id=ADMINS[0], text=msg)
+
+    except sqlite3.IntegrityError as err:
+        # await bot.send_message(chat_id=ADMINS[0], text=err)
+        pass
     await message.answer(f"Salom, {message.from_user.full_name}!")
